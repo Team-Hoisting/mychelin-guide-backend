2be23d9bb495cebb6f935b8aad4b8d39071ed75c
minji99710 <alswl99710@gmail.com>
2023-04-23 19:07:53 +0900

    Fix : comment 모델 이메일 수정

--- routes/comments.js
+++ routes/comments.js
@@ -8,6 +8,7 @@ router.get('/:storeid', (req, res) => {
   const storeComments = comments.findCommentsByStoreId(req.params.storeid);
 
   const commentUser = storeComments.map((comment) => {
+    console.log('comment email: ', comment.email);
     const { nickname, isCertified } = users.findUserByEmail(comment.email);
     return { ...comment, nickname, isCertified };
   });
