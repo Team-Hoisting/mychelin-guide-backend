a1eccf1f8ad84d306281f8e627b7c6ae5eb811b9
Kyu Sung Sohn <rok.ksohn@gmail.com>
2023-04-25 15:28:50 +0900

    Feat: '/api/store' query string 관련 로직 추가

--- routes/stores.js
+++ routes/stores.js
@@ -7,9 +7,22 @@ const archives = require('../models/archives');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const data = stores.getRankedStores(votes.getVotes());
+  const { category, page_size, page } = req.query;
 
-  res.send(data);
+  const rankedStores = stores.getRankedStores(votes.getVotes());
+
+  const startIndex = (page - 1) * page_size;
+  const endIndex = startIndex + +page_size;
+
+  const filteredStores = category
+    ? rankedStores.filter(({ votesByCategory }) =>
+        Object.keys(votesByCategory).includes(category),
+      )
+    : rankedStores;
+
+  const pageStores = filteredStores.slice(startIndex, endIndex);
+
+  res.send(pageStores);
 });
 
 router.get('/search', (req, res) => {
