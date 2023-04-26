e6a03db859f6129770b226b414b37a31e74d87b2
bin000527 <bin000527@sookmyung.ac.kr>
2023-04-26 19:56:05 +0900

    refactor: user/:nickname  > user/all/:nickname api

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const users = require('../models/users');
+const votes = require('../models/votes');
+const archives = require('../models/archives');
+
+const { findStoreById } = require('../models/stores');
 
 const router = express.Router();
 
-router.get('/:nickname', (req, res) => {
-  const data = users.findByNickname(req.params.nickname);
-  res.send(data);
+router.get('/all/:nickname', (req, res) => {
+  const user = users.findByNickname(req.params.nickname);
+
+  const votesByUser = votes.findVotesByEmail(user.email);
+
+  const voteStores = votesByUser.map(({ categoryCode, storeId, votedAt }) => ({
+    categoryCode,
+    store: findStoreById(storeId),
+    votedAt,
+  }));
+
+  const archivesByUser = archives.getArchivesByEmail(user.email);
+
+  const archiveStores = archivesByUser.map(({ storeId }) =>
+    findStoreById(storeId),
+  );
+
+  res.send({
+    user,
+    voteStores,
+    archiveStores,
+  });
 });
 
 module.exports = router;
